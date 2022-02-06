26fb78c6b7ef03ef9a653c5496ab7df38864de69
zayyid <mochamadzayyid@gmail.com>
2022-02-05 16:20:18 -0800

    tambah API

--- src/App.js
+++ src/App.js
@@ -4,7 +4,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      items: []
+    }
+  }
+
+componentDidMount(){
+  fetch("https://api.rajaongkir.com/starter/province",{
+    headers: {
+      "X-Auth-Token": "ae39bb9151609f3c5786bc1960064dac",
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => response.json() )
+    .then( data => this.setState( {items : data}))
+}
+
   render(){
+    const { items } = this.state
+
     return (
       <div className="App">
         <Navbar bg="light" expand="lg">
@@ -19,7 +39,13 @@ class App extends React.Component{
             </Navbar.Collapse>
           </Container>
         </Navbar>
-        <h1>hello</h1>
+        <ul>
+          {
+            items.map((item, index) =>
+              <li key={index}>{item}</li>
+            )
+          }
+        </ul>
       </div>
     );
   }
